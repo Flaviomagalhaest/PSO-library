cb6054a4fae599943c446dfe55c4276cc74ec0b2
Flavio <flaviomagalhaest@gmail.com>
2018-04-02 22:53:57 -0300

    Criação do método toJson()

--- tsp/PontosClass.py
+++ tsp/PontosClass.py
@@ -1,5 +1,6 @@
 from .PontoClass import Ponto
 import math
+import json
 
 class Pontos(object):
     pontos = []                                     #Lista de objetos 'Ponto'.
@@ -10,6 +11,7 @@ class Pontos(object):
         for p in dictPontos:        #Foreach para cada coordenada da lista
             ponto = Ponto(p)        #Instanciando classe Ponto através da coordenada x e y obtida
             self.pontos.append(ponto)       #Adicionado objeto Ponto na lista
+
     def calcMatrixDist(self):
         for p1 in range(0,len(self.pontos)):        #loop primário
             for p2 in range(0,len(self.pontos)):    #loop secundário da mesma lista de pontos
@@ -20,3 +22,14 @@ class Pontos(object):
                 distancia = math.hypot(x2 - x1, y2 - y1)    #cálculo de distância (Reta)
                 self.pontos[p1].matrixDist.append(distancia)    #Salvando distância na lista de matriz distância
                                                                 #Do objeto ponto em questão
+    def toJson(self):
+        jsonRetorno = []
+        for p1 in range(0,len(self.pontos)):
+            pontoRetorno = ''
+            pontoRetorno = {
+                'x':self.pontos[p1].x,
+                'y':self.pontos[p1].y,
+                'matrix':self.pontos[p1].matrixDist
+            }
+            jsonRetorno.append(pontoRetorno)
+        return json.dumps(jsonRetorno)
