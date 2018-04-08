932dd63059ae7b2f27bc655d31a0799b24441990
Flavio <flaviomagalhaest@gmail.com>
2018-04-07 23:01:24 -0300

    Criação de método novo na classe de Pontos, para que seja calculado o caminho passado para o objeto. Criado teste unitário para validar o método

--- tsp/PontosClass.py
+++ tsp/PontosClass.py
@@ -22,6 +22,17 @@ class Pontos(object):
                 distancia = math.hypot(x2 - x1, y2 - y1)    #cálculo de distância (Reta)
                 self.pontos[p1].matrixDist.append(distancia)    #Salvando distância na lista de matriz distância
                                                                 #Do objeto ponto em questão
+    
+    #Recebe uma lista de Pontos (caminho) e retorna a distância entre eles
+    def calcDistDeCaminhos(self, caminho):
+        dist = 0
+        for i in range(0, len(caminho)):    #Loop para checar toda a lista de inteiros (sequencia de pontos)
+            if (i == len(caminho) - 1):     #Caso esteja no último termo
+                dist += self.pontos[caminho[i]].matrixDist[caminho[0]]  #Soma a distância do último ponto ao primeiro.
+                return dist
+            dist += self.pontos[caminho[i]].matrixDist[caminho[i + 1]]  #Soma a distância total, a distância do ponto ao próximo
+        return 'Erro em calcular distancia total do caminho'
+
     def toJson(self):
         jsonRetorno = []
         for p1 in range(0,len(self.pontos)):
