02a08bacb3017438702f61a3190fe81f0a029bc8
Flavio <flaviomagalhaest@gmail.com>
2018-04-22 23:18:54 -0300

    [PSO-library] Alteração da controller para geração de iterações.

--- tsp/IndividuoClass.py
+++ tsp/IndividuoClass.py
@@ -8,9 +8,16 @@ class Individuo(object):
     gbest = False
 
     # Instância atual e distância do Individuo.
-    def __init__(self, atual, distAtual):
-        self.atual = atual
-        self.distAtual = distAtual
+    def __init__(self, atual=None, distAtual=None, json=None):
+        if(json == None):
+            self.atual = atual
+            self.distAtual = distAtual
+        else:
+            self.atual = json['atual']
+            self.pbest = json['pbest']
+            self.distAtual = json['distAtual']
+            self.distPbest = json['distPbest']
+            self.gbest = json['gbest']
     
     # Retornando objeto em json.
     def toJson(self):
--- tsp/tspController.py
+++ tsp/tspController.py
@@ -1,6 +1,6 @@
 from py.tsp import PontosClass as P
 from py.tsp import IndividuoClass as I
-import random
+import random, json
 
 def calcMatrixDist(pontosJson):
     pontosObj = P.Pontos(dictPontos = pontosJson)    #Instanciando classe de Pontos. Passando dicionario de coord.
@@ -12,7 +12,20 @@ def geraPopInicial(qtdPopInicial, qtdPontos, pontosJson):
     pontos = P.Pontos(jsonSessao=pontosJson)    #Instanciando classe pontos passando string json.
     for i in range(0,qtdPopInicial):    #Loop para criar individuos dependendo da quantidade passada.
         caminhoRandom = random.sample(range(qtdPontos),qtdPontos)   #Criando lista de numeros aleatorios (caminho).
+        caminhoRandom.append(caminhoRandom[0])  #Adicionando primeiro termo no final. O caminho tem que ser ciclico.
         distanciaCaminho = pontos.calcDistDeCaminhos(caminhoRandom) #Calculando distancia.
         indiv = I.Individuo(caminhoRandom, distanciaCaminho)        #Instanciando classe Individuo passando caminho e distancia.
         individuos.append(indiv)   #Adicionando individuo instanciado na lista.
     return individuos
+
+def geraIteracao(iteracaoAtual, nrIteracoes, individuosJson):
+    individuos = []    #Lista de individuos.
+    for i in range(0,len(individuosJson)): #Itera lista de individuos em Json para instanciar no objeto individuos
+        individuo = I.Individuo(json=json.loads(individuosJson[i]))
+        individuos.append(individuo)    #Lista de individuos já instanciados
+    
+    #Começando iterações
+    for j in range(0,nrIteracoes):
+        #Calculando constantes k e m
+        k = randint(0,)   
+    a = 1
