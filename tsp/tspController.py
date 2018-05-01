72217b9b9557ea4839cd4962cf1bd5c9617863bf
Flavio <flaviomagalhaest@gmail.com>
2018-05-01 11:05:06 -0300

    [PSO-library] Finalizando iteração com cross-over

--- tsp/tspController.py
+++ tsp/tspController.py
@@ -19,16 +19,16 @@ def geraPopInicial(qtdPopInicial, qtdPontos, pontosJson):
     individuos = atualizaGbest(individuos)
     return individuos
 
-def geraIteracao(iteracaoAtual, nrIteracoes, individuosJson):
+def geraIteracao(iteracaoAtual, nrIteracoes, individuosJson, pontosJson):
     individuos = []    #Lista de individuos.
+    pontos = P.Pontos(jsonSessao = pontosJson)    #Instanciando classe pontos passando string json.
     for i in range(0,len(individuosJson)): #Itera lista de individuos em Json para instanciar no objeto individuos
         individuo = I.Individuo(json=json.loads(individuosJson[i]))
         individuos.append(individuo)    #Lista de individuos já instanciados
     
     #Começando iterações
     for j in range(0,nrIteracoes):
-        gbest = atualizaGbest(individuos)       #atualizando gbest
-        individuos = atualizaPbest(individuos)  #atualizando pbest
+        gbest = [i for i in individuos if i.gbest == True]  #Buscando o gbest da lista de individuos
 
         #Iterar cada individuo para crossover
         for indiv in individuos:
@@ -36,13 +36,20 @@ def geraIteracao(iteracaoAtual, nrIteracoes, individuosJson):
             #Gerando constantes para calculo incluindo pBest
             kPbest = random.randint(0, qtdPontos - 1)
             mPbest = random.randint(kPbest + 1, qtdPontos)
-            
             #Gerando constantes para calculo incluindo gBest
             kGbest = random.randint(0, qtdPontos - 1)
-            mGbest = random.randint(kPbest + 1, qtdPontos)
+            mGbest = random.randint(kGbest + 1, qtdPontos)
+            #Fazendo crossover do caminho atual com o pbest
+            indiv.atual = crossOver(indiv.atual, indiv.pbest, kPbest, mPbest)
+            #Fazendo crossover do caminho atual com o gbest
+            indiv.atual = crossOver(indiv.atual, gbest[0].atual, kGbest, mGbest)
+            #Calculando nova distancia do individuo
+            indiv.distAtual = pontos.calcDistDeCaminhos(indiv.atual)
 
-
-    a = 1
+        gbest = atualizaGbest(individuos)       #atualizando gbest
+        individuos = atualizaPbest(individuos)  #atualizando pbest
+    
+    return individuos
 
 def atualizaGbest(individuos):
     distGbest = 9999999999
@@ -52,6 +59,7 @@ def atualizaGbest(individuos):
         if indiv.distAtual < distGbest: #Verificando se o individuo tem uma distancia menor que o gbest
             distGbest = copy.copy(indiv.distAtual)  #Atualizando melhor distancia achada
             gbest = indice  #guardando indice do melhor individuo ate agora
+        indiv.gbest = False #Resetando flag de gbest para o individuo
         indice += 1
     individuos[gbest].gbest = True  #Setando flag de gbest no individuo onde o índice foi salvo
     return individuos   #retornando lista de individuos com a flag de gbest setada para o melhor deles.
@@ -68,4 +76,6 @@ def crossOver(caminhoDestino, caminhoBest, k, m):
     caminhoDestino = caminhoDestino[0 : (len(caminhoDestino) - 1)] #Retirando último ponto da lista.
     caminhoRetorno = P + caminhoDestino
     seen = set()
-    return [x for x in caminhoRetorno if not (x in seen or seen.add(x))] 
+    caminhoRetorno = [x for x in caminhoRetorno if not (x in seen or seen.add(x))]  #Limpando a lista deixando apenas pontos distintos
+    caminhoRetorno.append(caminhoRetorno[0])
+    return caminhoRetorno
